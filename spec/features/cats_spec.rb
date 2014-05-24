e7d565d5168658eace3feae6af62143e8489b225
Nathanael Burt <nathanael.burt@gmail.com>
2014-05-24 10:50:48 -0600

    user can create cats

--- spec/features/cats_spec.rb
+++ spec/features/cats_spec.rb
@@ -4,9 +4,18 @@ feature 'crudding cats' do
 
   scenario 'a user can visit the index' do
     visit '/'
-    within 'h1' do
-      expect(page).to have_content 'Cats!'
-    end
+    expect(page).to have_content 'Cats!'
+  end
+
+  scenario 'a user can create a cat' do
+    visit '/'
+    click_on 'Cats!'
+    click_on 'Add Cat'
+    fill_in 'cat[name]', with: 'Spot'
+    fill_in 'cat[color]', with: 'brown'
+    click_on 'Create Cat'
+    expect(page).to have_content 'Spot'
+    expect(page).to have_content 'brown'
   end
 
 end
